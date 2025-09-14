52e09f418a37569ff8e92d44bfcccae0d0f1eb21
Masum Billah Tarek <mdmasumbillah245524@gmail.com>
2025-09-14 11:46:12 +0300

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,105 @@
 const express=require('express')
+require('dotenv').config()
 const app=express()
 const cors=require('cors')
 const port=process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
+
+//SXS4osQ4TiNXo8sU
+//eventHive
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0h4513.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+  const database=client.db('EventHive')
+  const userCollection=database.collection('users')
+   const eventCollection=database.collection('events')
+
+app.post('/events',async(req,res)=>{
+  const event=req.body
+  const result=await eventCollection.insertOne(event)
+  res.send(result)
+})
+app.get('/events',async(req,res)=>{
+    const cursor=eventCollection.find()
+    const result=await cursor.toArray()
+    res.send(result)
+})
+
+  app.post('/users',async(req,res)=>{
+    const user=req.body
+    const query={email:user.email}
+    const existingUser=await userCollection.findOne(query)
+    if(existingUser){
+        return res.send({message:'User already exists',insertedId:null})
+    }
+    const result=await userCollection.insertOne(user)
+    res.send(result)
+  })
+  app.patch('/users/:id',async(req,res)=>{
+    const id=req.params.id
+    const query={_id:new ObjectId(id)}
+    const updateDoc={
+        $set:{
+            role:'admin'
+        }
+    }
+    const result=await userCollection.updateOne(query,updateDoc)
+    res.send(result)
+  })
+  app.get('/users/:id',async(req,res)=>{
+    const id=req.params.id;
+   const query={_id:new ObjectId(id)}
+   const result=await userCollection.findOne(query)
+   res.send(result)
+  })
+  app.delete('/users/:id',async(req,res)=>{
+        const id=req.params.id;
+   const query={_id:new ObjectId(id)}
+   const result=await userCollection.deleteOne(query)
+   res.send(result)
+  })
+  app.get('/users/admin/:email',async(req,res)=>{
+     const email=req.params.email
+    const query={email:email}
+    const user=await userCollection.findOne(query)
+    let admin=false;
+    if(user){
+        admin=user?.role==='admin'
+    }
+    res.send({admin})
+  })
+app.get('/users',async(req,res)=>{
+    const cursor=userCollection.find()
+    const result=await cursor.toArray()
+    res.send(result)
+})
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',(req,res)=>{
     res.send('Event Hive is starting')
 })
